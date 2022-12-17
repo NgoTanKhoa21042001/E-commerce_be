f1d68c5a1874575af27f1744f216b0ff86def93e
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2022-12-17 09:35:25 +0700

    Create server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: "*",
+  method: "*",
+});
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+server.listen(8081, () => {
+  console.log("server listening running...", 8081);
+});
