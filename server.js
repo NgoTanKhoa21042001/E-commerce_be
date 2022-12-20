9ff5265932d6f3214af95b6d420ccbd7936a9a75
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2022-12-20 10:24:35 +0700

    Connect to db

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const http = require("http");
+require("./connection");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
