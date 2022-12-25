0363d7bf4430620a82bed874629c00dbca826744
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2022-12-25 20:10:21 +0700

    Create User model

--- connection.js
+++ connection.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const connectionStr = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@cluster0.p7xwa58.mongodb.net/ecomern?retryWrites=true&w=majority`;
 
 mongoose
-  .connect(connectionStr, { useNewUrlParser: true })
+  .connect(connectionStr, { useNewUrlparser: true })
   .then((data) => console.log("connected to mongod"))
   .catch((err) => console.log(err));
 
