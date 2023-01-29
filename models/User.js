ca9205b46d20efd49f43674f68536d4d1619434f
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-01-29 18:49:26 +0700

    hash the password

--- models/User.js
+++ models/User.js
@@ -63,5 +63,23 @@ UserSchema.methods.toJSON = function () {
   return userObject;
 };
 
+// hash the password
+
+UserSchema.pre("save", function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) return next();
+
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
 const User = mongoose.model("User", UserSchema);
 module.exports = User;
