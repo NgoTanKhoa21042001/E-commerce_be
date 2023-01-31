3746e0304da44be508af26ebd58c4fa62b497a09
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-01-31 20:54:00 +0700

    Handle Error on product

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,32 +1,30 @@
 const Product = require("../models/ProductModel");
-
+const ErrorHandler = require("../utils/ErrorHandler");
+const catchAsynErrors = require("../middleware/catchAsynError");
 /// create product
 
-exports.createProduct = async (req, res, next) => {
+exports.createProduct = catchAsynErrors(async (req, res, next) => {
   const product = await Product.create(req.body);
   res.status(201).json({
     success: true,
     product,
   });
-};
+});
 // get all products
-exports.getAllProducts = async (req, res) => {
+exports.getAllProducts = catchAsynErrors(async (req, res) => {
   const products = await Product.find();
   res.status(200).json({
     success: true,
     products,
   });
-};
+});
 
 // Update products -- Admin
 
-exports.updateProduct = async (req, res, next) => {
+exports.updateProduct = catchAsynErrors(async (req, res, next) => {
   let product = await Product.findById(req.params.id);
   if (!product) {
-    return res.status(500).json({
-      success: false,
-      message: "Product not found with this id",
-    });
+    return next(new ErrorHandler("Product is not found with this id", 404));
   }
   product = await Product.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
@@ -37,20 +35,31 @@ exports.updateProduct = async (req, res, next) => {
     success: true,
     product,
   });
-};
+});
 
 // Delete Product
-exports.deleteProduct = async (req, res, next) => {
+exports.deleteProduct = catchAsynErrors(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
   if (!product) {
-    return res.status(500).json({
-      success: false,
-      message: "Product not found with this id",
-    });
+    return next(new ErrorHandler("Product is not found with this id", 404));
   }
   await product.remove();
   res.status(200).json({
     success: true,
     message: "Product deleted successfully",
   });
-};
+});
+
+// single product details
+
+exports.getSingleProduct = catchAsynErrors(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  console.log(product);
+  if (!product) {
+    return next(new ErrorHandler("Product is not found with this id", 404));
+  }
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
