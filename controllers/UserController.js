5bb6e9da36e5c36a35bb903ee07aadd96daeb722
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-02-02 11:49:59 +0700

    Update password and user profile successfully

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -153,3 +153,40 @@ exports.userDetails = catchAsynErrors(async (req, res, next) => {
     user,
   });
 });
+
+// Update user password
+exports.updatePassword = catchAsynErrors(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select("+password");
+
+  const isPasswordMatched = await user.comparePassword(req.body.oldPassword);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Old password is incorrect", 400));
+  }
+
+  if (req.body.newPassword !== req.body.confirmPassword) {
+    return next(
+      new ErrorHandler("Password not matched with each other  ", 400)
+    );
+  }
+  user.password = req.body.newPassword;
+  await user.save();
+  sendToken(user, 200, res);
+});
+
+// Update User Profile
+exports.updateProfile = catchAsynErrors(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+  // we add cloudinary letter then we are giving condition for the
+  const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({
+    success: true,
+  });
+});
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -6,6 +6,8 @@ const {
   forgotPassword,
   resetPassword,
   userDetails,
+  updatePassword,
+  updateProfile,
 } = require("../controllers/UserController");
 const { isAuthenticatedUser } = require("../middleware/auth");
 const router = express.Router();
@@ -15,5 +17,7 @@ router.route("/login").post(loginUser);
 router.route("/logout").get(logoutUser);
 router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
+router.route("/me/update").put(isAuthenticatedUser, updatePassword);
+router.route("/me/update/info").put(isAuthenticatedUser, updateProfile);
 router.route("/me").get(isAuthenticatedUser, userDetails);
 module.exports = router;
