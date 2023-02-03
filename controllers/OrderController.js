7f400fe0f1105b86023cded7036f37ad61815c7a
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2023-02-03 09:31:38 +0700

    Create new order

---
+++ controllers/OrderController.js
@@ -0,0 +1,32 @@
+const Order = require("../models/OrderModel");
+const ErrorHandler = require("../utils/ErrorHandler");
+const catchAsynErrors = require("../middleware/catchAsynErrors");
+
+// Create Order
+exports.createOrder = catchAsynErrors(async (req, res, next) => {
+  const {
+    shippingInfo,
+    orderItems,
+    paymentInfo,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  const order = await Order.create({
+    shippingInfo,
+    orderItems,
+    paymentInfo,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    paidAt: Date.now(),
+    user: req.user._id,
+  });
+  res.status(201).json({
+    success: true,
+    order,
+  });
+});
